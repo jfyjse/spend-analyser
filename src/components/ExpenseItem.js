53b02e532033f35775df4f1367a02479575d0b5c
Joffy Jose <joffy.jose.jfy@gmail.com>
2021-12-16 18:01:53 +0530

    basics added

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+import './ExpenseItem.css'
+
+function ExpenseItem(){
+
+    return(
+        <div className='expense-item'>
+            <div>Item</div>
+            <div className='expense-item__description'>descr</div>
+
+        </div>
+    )
+}
+export default ExpenseItem;
