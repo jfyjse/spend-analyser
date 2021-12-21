680b4f8f176cacf584cc5280e063c52d7bf81283
Joffy Jose <joffy.jose.jfy@gmail.com>
2021-12-21 18:14:07 +0530

    event listners, states into

--- src/components/Expense/ExpenseItem.js
+++ src/components/Expense/ExpenseItem.js
@@ -1,14 +1,22 @@
 import "./ExpenseItem.css";
+import React, {useState} from "react";
 import ExpenseDate from "../Expense/ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
+  const [title,setTitle] = useState(props.title)
+  const clickHandler =()=>{
+    setTitle('updated..!!');
+    alert(title)
+  }
   return (
+  
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
+        <button onClick={clickHandler}>click me..!!</button>
       </div>
     </Card>
   );
