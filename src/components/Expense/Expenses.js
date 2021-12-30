bbb9450217869bf1221cf46013f688fe28906fa3
Joffy Jose <joffy.jose.jfy@gmail.com>
2021-12-30 18:30:44 +0530

    dynamic lists, filter date, keys

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,32 +1,33 @@
 import "./Expenses.css";
+import React, { useState } from "react";
 import ExpenseItem from "../Expense/ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
-  
+  const [filteredYear, setFilteredYear] = useState("2021");
+  const filterChangedHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChanged={filterChangedHandler}
+        />
+
+        {props.items.map((expens) => (
+          <ExpenseItem
+            key={expens.id}
+            title={expens.title}
+            amount={expens.amount}
+            date={expens.date}
+          />
+        ))}
+      </Card>
+    </div>
   );
-}
+};
 export default Expenses;
